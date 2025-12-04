1a198b25d216088f025ec5fba5bf478ad115fd17
shun2f26 <shun2f26@gmail.com>
2025-12-04 15:03:21 +0900

    Update myblog.py

--- myblog.py
+++ myblog.py
@@ -457,6 +457,34 @@ def admin():
 
     return render_template("admin.html", post_data=post_data, is_admin=current_user.is_admin)
 
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash("ログイン成功しました！", "success")
+            return redirect(url_for("index"))
+        else:
+            flash("メールアドレスまたはパスワードが違います。", "danger")
+
+    return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("ログアウトしました。", "info")
+    return redirect(url_for("index"))
 
 # -------------------------------------------------------
 #  Error Pages
